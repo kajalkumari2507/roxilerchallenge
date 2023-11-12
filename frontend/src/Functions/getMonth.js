fbc32c65be1c8319ea0bbc67ae86313c32660e3e
Kajal Kumari <114283323+kajalkumari2507@users.noreply.github.com>
2023-11-12 23:36:13 +0530

    Delete frontend directory

--- frontend/src/Functions/getMonth.js
+++
@@ -1,9 +0,0 @@
-const getMonth = (month) =>{
-    const months = ['January', 'February', 'March',
-                    'April', 'May', 'June',
-                    'July', 'August', 'September',
-                    'October', 'November', 'December'];
-    return months[month-1];
-}
-
-export default getMonth;
